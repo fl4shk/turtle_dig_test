15a62856e273234484bb4cb2e25dd6cb77c35f16
fl4shk <fl4shk@users.noreply.github.com>
2016-01-08 10:29:29 -0600

    Small optimization

--- dig_test.lua
+++ dig_test.lua
@@ -27,8 +27,19 @@ local function_type_str = "function"
 
 local dir_positive_x, dir_positive_y = 0, 1
 local dir_negative_x, dir_negative_y = 2, 3
-local pos_index_x, pos_index_y, pos_index_z = 1, 2, 3
 local up_str, down_str = "up", "down"
+
+vec3_prototype = { x = 0, y = 0, z = 0 }
+
+function vec3_prototype.__init__( x, y, z )
+	local self = { x = x, y = y, z = z }
+	setmetatable( self, {__index = vec3_prototype} )
+	return self
+end
+
+function vec3_prototype:get_size()
+	return 3
+end
 
 function test_func( a )
 	print(type(a))
@@ -56,13 +67,13 @@ end
 
 function dig_then_move_forward( faced_dir, pos )
 	if ( faced_dir[1] == dir_positive_x ) then
-		pos[pos_index_x] = pos[pos_index_x] + 1
+		pos.x = pos.x + 1
 	elseif ( faced_dir[1] == dir_positive_y ) then
-		pos[pos_index_y] = pos[pos_index_y] + 1
+		pos.y = pos.y + 1
 	elseif ( faced_dir[1] == dir_negative_x ) then
-		pos[pos_index_x] = pos[pos_index_x] - 1
+		pos.x = pos.x - 1
 	else --if ( faced_dir[1] == dir_negative_y ) then
-		pos[pos_index_y] = pos[pos_index_y] - 1
+		pos.y = pos.y - 1
 	end
 	
 	turtle.dig()
@@ -70,7 +81,7 @@ function dig_then_move_forward( faced_dir, pos )
 end
 
 function turn_and_move_at_end_of_row( faced_dir, pos )
-	if ( ( pos[pos_index_z] % 2 ) == 0 ) then
+	if ( ( pos.z % 2 ) == 0 ) then
 		if ( faced_dir[1] == dir_positive_x ) then
 			turn_right(faced_dir)
 			dig_then_move_forward( faced_dir, pos )
@@ -82,7 +93,7 @@ function turn_and_move_at_end_of_row( faced_dir, pos )
 			turn_left(faced_dir)
 			faced_dir = dir_negative_x
 		end
-	else --if ( ( pos[pos_index_z] % 2 ) == 1 ) then
+	else --if ( ( pos.z % 2 ) == 1 ) then
 		if ( faced_dir[1] == dir_positive_x ) then
 			turn_left(faced_dir)
 			dig_then_move_forward( faced_dir, pos )
@@ -98,64 +109,84 @@ function turn_and_move_at_end_of_row( faced_dir, pos )
 end
 
 function dig_up_then_move_up( faced_dir, pos )
-	
-	pos[pos_index_z] = pos[pos_index_z] + 1
+	pos.z = pos.z + 1
 	
 	turtle.digUp()
 	turtle.up()
+end
+
+function dig_up_then_move_up_with_turn( faced_dir, pos )
+	dig_up_then_move_up( faced_dir, pos )
 	
-	if ( ( pos[pos_index_z] % 2 ) == 0 ) then
+	if ( ( pos.z % 2 ) == 0 ) then
 		turn_left(faced_dir)
 		turn_left(faced_dir)
-	else --if ( ( pos[pos_index_z] % 2 ) == 1 ) then
+	else --if ( ( pos.z % 2 ) == 1 ) then
 		turn_right(faced_dir)
 		turn_right(faced_dir)
 	end
-	
 end
 
 function dig_down_then_move_down( faced_dir, pos )
-	
-	pos[pos_index_z] = pos[pos_index_z] - 1
+	pos.z = pos.z - 1
 	
 	turtle.digDown()
 	turtle.down()
+end
+
+function dig_down_then_move_down_with_turn( faced_dir, pos )
+	dig_down_then_move_down( faced_dir, pos )
 	
-	if ( ( pos[pos_index_z] % 2 ) == 0 ) then
+	if ( ( pos.z % 2 ) == 0 ) then
 		turn_left(faced_dir)
 		turn_left(faced_dir)
-	else --if ( ( pos[pos_index_z] % 2 ) == 1 ) then
+	else --if ( ( pos.z % 2 ) == 1 ) then
 		turn_right(faced_dir)
 		turn_right(faced_dir)
 	end
-	
 end
 
 function main()
 	if ( #args < 3 or #args > 4 ) then
 		print("Usage:  <program name> num_blocks_x num_blocks_y "
-			.. "num_blocks_z (bool_move_up)")
+			.. "num_blocks_z (up or down)")
 		return
 	end
 	
 	-- Three entries.
-	local num_blocks_tbl = { 0, 0, 0 }
+	local num_blocks_vec = vec3_prototype.__init__( 0, 0, 0 )
 	
-	for i = 1, #num_blocks_tbl do
-		num_blocks_tbl[i] = tonumber(args[i])
-		
-		if ( num_blocks_tbl[i] == nil ) then
-			print("Error!  The argument \"" .. args[i] .. "\" is not a "
-				.. "valid number!")
-			return
-		end
+	-- x
+	num_blocks_vec.x = tonumber(args[1])
+	if ( num_blocks_vec.x == nil ) then
+		print("Error!  The argument \"" .. args[1] .. "\" is not a "
+			.. "valid number!")
+		return
 	end
+	
+	-- y
+	num_blocks_vec.y = tonumber(args[2])
+	if ( num_blocks_vec.y == nil ) then
+		print("Error!  The argument \"" .. args[2] .. "\" is not a "
+			.. "valid number!")
+		return
+	end
+	
+	-- z
+	num_blocks_vec.z = tonumber(args[3])
+	if ( num_blocks_vec.z == nil ) then
+		print("Error!  The argument \"" .. args[3] .. "\" is not a "
+			.. "valid number!")
+		return
+	end
+	
+	
 	
 	-- The faced direction
 	local faced_dir = { dir_positive_x }
 	
 	-- The position with respect to the initial position.
-	local pos = { 0, 0, 0 }
+	local pos = vec3_prototype.__init__( 0, 0, 0 )
 	
 	local up_or_down
 	
@@ -175,11 +206,11 @@ function main()
 	end
 	
 	
-	for k = 1, num_blocks_tbl[pos_index_z] do
+	for k = 1, num_blocks_vec.z do
 		
-		for j = 2, num_blocks_tbl[pos_index_y] do
+		for j = 2, num_blocks_vec.y do
 			
-			for i = 2, num_blocks_tbl[pos_index_x] do
+			for i = 2, num_blocks_vec.x do
 				dig_then_move_forward( faced_dir, pos )
 			end
 			
@@ -187,15 +218,25 @@ function main()
 			
 		end
 		
-		for i = 2, num_blocks_tbl[pos_index_x] do
+		for i = 2, num_blocks_vec.x do
 			dig_then_move_forward( faced_dir, pos )
 		end
 		
-		if ( k < num_blocks_tbl[pos_index_z] ) then
-			if ( up_or_down == up_str ) then
-				dig_up_then_move_up( faced_dir, pos )
-			elseif ( up_or_down == down_str ) then
-				dig_down_then_move_down( faced_dir, pos )
+		if ( num_blocks_vec.x == 1 and num_blocks_vec.y == 1 ) then
+			if ( k < num_blocks_vec.z ) then
+				if ( up_or_down == up_str ) then
+					dig_up_then_move_up( faced_dir, pos )
+				elseif ( up_or_down == down_str ) then
+					dig_down_then_move_down( faced_dir, pos )
+				end
+			end
+		else
+			if ( k < num_blocks_vec.z ) then
+				if ( up_or_down == up_str ) then
+					dig_up_then_move_up_with_turn( faced_dir, pos )
+				elseif ( up_or_down == down_str ) then
+					dig_down_then_move_down_with_turn( faced_dir, pos )
+				end
 			end
 		end
 		
